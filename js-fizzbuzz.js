c710d134cef9fc9f6ced92a2fd64defb73a75930
Giovanni-Iapichino <giovanniiapichino28@gmail.com>
2025-04-02 14:04:56 +0200

    first commit

---
+++ js-fizzbuzz.js
@@ -0,0 +1,7 @@
+// Scrivi un programma che stampi i numeri da 1 a 100,
+// - per i multipli di 3 stampi "Fizz" al posto del numero
+// - per i multipli di 5 stampi "Buzz" al posto del numero
+// - per i multipli di 3 e di 5 stampi "FizzBuzz" al posto del numero
+// Prima di partire a scrivere codice poniamoci qualche domanda:
+// Come faccio a sapere se un numero è divisibile per un altro? Abbiamo visto qualcosa di particolare
+// che possiamo usare?
