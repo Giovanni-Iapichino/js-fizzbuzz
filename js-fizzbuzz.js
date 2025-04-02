0e6c44e3d88bcf8c831006f578d5b1ec7b969984
Giovanni-Iapichino <giovanniiapichino28@gmail.com>
2025-04-02 15:02:07 +0200

    add-fizzbuzz

--- js-fizzbuzz.js
+++ js-fizzbuzz.js
@@ -5,3 +5,20 @@
 // Prima di partire a scrivere codice poniamoci qualche domanda:
 // Come faccio a sapere se un numero è divisibile per un altro? Abbiamo visto qualcosa di particolare
 // che possiamo usare?
+
+// STAMPO UN PROGRAMMA CHE CONTA I NUMERI DA 1 A 100
+for (let i = 1; i <= 100; i++) {
+  // PER I MULTIPLI DI 3 E 5 STAMPO "FIZZBUZZ" AL POSTO DEL NUMERO
+  if (i % 15 == 0) {
+    console.log("FizzBuzz");
+    // PER I MULTIPLI DI 3 STAMPO "FIZZ" AL POSTO DEL NUMERO
+  } else if (i % 3 == 0) {
+    console.log("Fizz");
+    // PER I MULTIPLI DI 5 STAMPO "BUZZ" AL POSTO DEL NUMERO
+  } else if (i % 5 == 0) {
+    console.log("Buzz");
+    // PER I NUMERI CHE NON SONO NE MULTIPLI DI 3 NE MULTIPLI DI 5 STAMPO IL LORO REALE VALORE
+  } else {
+    console.log(i);
+  }
+}
